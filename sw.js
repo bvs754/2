53542f2f961ae75a83d434b9412d12b766205d8a
bvs777 <57488904+bvs754@users.noreply.github.com>
2019-11-11 20:01:06 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -72,7 +72,7 @@ toolbox.precache([
        "/js/jquery-1.12.4.min.js"
        ]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 10
